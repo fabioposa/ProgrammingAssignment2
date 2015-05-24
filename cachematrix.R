86e2711d5706386e6de5ab3bb21470f0602a2194
fabioposa <fabio.posa@libero.it>
2015-05-24 22:12:29 +0200

    put more detail

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,41 @@
 ## funtion get give the origial input matrix
 ## setmatrix is call from cacheSolve to put in m (external variable) the inverse matrix.
 ## getinverse give the result "inverse matrix" is was call in cacheSolve 
+## Example of execution 
+## input_example<-matrix(2:5, nrow=2, ncol=2)
+## input_example
+##     [,1] [,2]
+## [1,]    2    4
+## [2,]    3    5
+## You can call 
+## example<-makeCacheMatrix(input_example)
+## example$get()         # Returns original matrix
+## example$get()
+##     [,1] [,2]
+## [1,]    2    4
+## [2,]    3    5
+##
+## First time get inverse not have setting (variale m) the result and return NULL
+## example$getinverse()
+## NULL
+## You can set using cacheSolve
+## cacheSolve(example)
+##    [,1] [,2]
+##[1,] -2.5    2
+##[2,]  1.5   -1
+## Now we have in m the inverse matrix in variable m
+## if we call example$getinverse() now is setting 
+##     [,1] [,2]
+##[1,] -2.5    2
+##[2,]  1.5   -1
+## But if we can call another time example$getinverse() the variable m is setting and tell that "getting cached data"
+## and don't calculate another time the inverse matrix, we just have in variable m
+## cacheSolve(example)
+##getting cached data
+##     [,1] [,2]
+##[1,] -2.5    2
+##[2,]  1.5   -1
+## if we call makeCacheMatrix  with another matrix we set m to null and we start as the first example.
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -11,10 +46,10 @@ makeCacheMatrix <- function(x = matrix()) {
                 m <<- NULL
         }
         get <- function() x
-        setmatrix <- function(matrix) m <<- matrix
+        setinverse <- function(matrix) m <<- matrix
         getinverse <- function() m
         list(set = set, get = get,
-             setmatrix = setmatrix,
+             setinverse = setinverse,
              getinverse = getinverse)
 
 }
@@ -33,6 +68,6 @@ cacheSolve <- function(x, ...) {
         }
         data <- x$get()
         m <- solve(data, ...)
-        x$setmatrix(m)
+        x$setinverse(m)
         m
 }
